f6ef598d04a41731ec3186ce8af3d6b63c9369d9
Jesus Glez <jesusgleztr94@gmail.com>
2024-03-04 18:07:52 -0600

    feat: :sparkles: Importacion de lotes por excel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use bfinlay\SpreadsheetSeeder\SpreadsheetSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,8 @@ class DatabaseSeeder extends Seeder
 		$this->call(PermissionSeeder::class);
 		$this->call(AdminSeeder::class);
 		// $this->call(LotesSeeder::class);
+		$this->call([
+			SpreadsheetSeeder::class,
+		]);
 	}
 }
